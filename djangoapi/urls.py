afbb2746825d36cf3115022cb5110cc700761ede
root <root@ip-172-31-38-23.us-west-2.compute.internal>
2018-12-07 19:02:19 +0000

    los ocupados no se muestran bien pero bueno

--- djangoapi/urls.py
+++ djangoapi/urls.py
@@ -9,8 +9,8 @@ from .views import CreateOcupadoView
 urlpatterns = [
 	url('usuarios', UsuariosList),
 	url('espacios', EspaciosList),
-	url('reservas', ReservasList),
-	url('reservas/create', CreateReservaView.as_view()),
+	url('reservas', ReservasList, name = 'reservas'),
+	url('create', CreateReservaView.as_view(), name = 'reservasCreate'),
 	url('parqueaderos', ParqueaderosList),
 	url('ocupados', CreateOcupadoView.as_view()),
 
